6bfe10c0f4cb0fb3163873aa0983445bf10461c8
Ravindra Shukla <ravindra.shukla@gmail.com>
2014-06-21 01:33:47 -0400

    assignment 2 file

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,30 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+## set m as NULL  
+  m <- NULL
+  
+## Set assigns arg to global vax x & NULL to m
+    sets <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+## get just get global var x
+    gets <- function() x
 
+## setmean assign arg to global var m
+    setmeans <- function(solve) m <<- solve
+
+## getmean just gets global m
+    getmeans <- function() m
+
+
+## output 
+list(sets = sets, gets = gets,
+         setmeans = setmeans,
+         getmeans = getmeans)
+  
 }
 
 
@@ -12,4 +35,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getmeans()
+    if(!is.null(m)) {
+      message("getting cached data")
+      return(m)
+    }
+    data <- x$gets()
+    m <- solve(data, ...)
+    x$setmeans(m)
+    
+    m
+  
 }
